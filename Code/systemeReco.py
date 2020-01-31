ff5f2f9b2fff02f8de775253dcdde5c19841a90c
anlaichu <chulai1997@gmail.com>
2020-01-31 14:05:32 +0100

    up

--- Code/systemeReco.py
+++ Code/systemeReco.py
@@ -14,6 +14,7 @@ import os
 
 # FUNCTION IMPORT
 import preference_consommateur as pref
+import motifs_frequents_substituabilite as motifs
 
 # =============================================================================
 
