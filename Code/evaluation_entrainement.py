32159ad6b1e4a60060fbafec1bb93fd4add53958
anlaichu <chulai1997@gmail.com>
2020-02-18 13:01:20 +0100

    up

--- Code/evaluation_entrainement.py
+++ Code/evaluation_entrainement.py
@@ -32,9 +32,10 @@ def taux_acceptation_df(data) :
     
     data = data[data['substitution'].str.len() > 2]
     data = data.groupby(['alpha', 'beta', 'omega_ini', 'seuil_acc', 'reponse'])['user'].count().reset_index().rename(
-            columns = {'user' : 'count_True'}).drop(
-                    'reponse', axis = 1)
+            columns = {'user' : 'count_True'})
     data['count_reponse'] = data.groupby(['alpha', 'beta', 'omega_ini', 'seuil_acc'])['count_True'].transform('sum')
+    data = data[data['reponse'] == True].drop(
+                    'reponse', axis = 1)
     data['taux_acceptation'] = round(100*data['count_True'] / data['count_reponse'], 2)
     
     data['alpha_beta'] = list(zip(data['alpha'], data['beta']))
