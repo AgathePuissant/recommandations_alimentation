ed01566c6d5624e4e69796e4cfd6c8dc0bf48811
anlaichu <chulai1997@gmail.com>
2020-02-18 10:59:02 +0100

    Update evaluation_entrainement.py

--- Code/evaluation_entrainement.py
+++ Code/evaluation_entrainement.py
@@ -4,4 +4,42 @@ Created on Tue Feb 18 10:43:31 2020
 
 @author: ADMIN
 """
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+data = pd.read_csv("Base_Gestion_Systeme/base_entrainement.csv", sep = ";", encoding = "latin-1")
+
+
+# =============================================================================
+# % RECOMMANDATIONS / REPAS PAR CLUSTER
+def manipulation1() :
+    pass
+
+def visualisation1() :
+    pass
+
+# =============================================================================
+
+
+# =============================================================================
+# % SUBSTITUTIONS ACCEPTÉES / PROPOSÉES PAR COEFF
+def manipulation2() :
+    pass
+
+def visualisation2() :
+    pass
+
+# =============================================================================
+
+
+# =============================================================================
+# AMÉLIORATION DU SCORE NUTRI PAR COEFF
+def manipulation3() :
+    pass
+
+def visualisation3() :
+    pass
+
+# =============================================================================
 
